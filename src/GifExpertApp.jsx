4bad8171530026a1934acbbffb8c388614145f88
Robeh <robeh0018@gmail.com>
2023-01-27 20:20:40 +0100

    First Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+// import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    const onAddCategory = (newCategory) => {
+        if(categories.includes(newCategory)) return;
+
+        setCategories((cat) => [newCategory, ...cat]);
+    };
+
+
+
+    return (
+       <>
+ 
+            <h1>GifExpertApp</h1>
+
+   
+            <AddCategory 
+                // setCategories = { setCategories }
+                onNewCategory={value => onAddCategory(value)}
+            />
+
+            <button onClick={ onAddCategory }>
+                Add
+            </button>
+    
+            { 
+                categories.map( category => (
+                        <GifGrid
+                            key={ category }
+                            category={ category }
+                        />
+                    ))
+            }
+
+       </>
+    );
+};
